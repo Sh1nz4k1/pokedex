3c2a2dcb74c28b7acf7061724bd106fb9a261f8b
Eduardo Porto <dudu05porto@gmail.com>
2023-08-28 15:34:27 -0300

    Melhorias JS e integração e modificação da API pública PokeApi

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,15 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
-  );
-}
-
 function convertPokemonHTML(pokemon) {
   return `
-    <li class="pokemon">
-      <span class="number">#${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
-              ${convertPokemonTypesToLi(pokemon.types).join("")}
+            ${pokemon.types
+              .map((type) => `<li class="type ${type}">${type}</li>`)
+              .join("")}
             </ol>
-          <img src="${pokemon.sprites.other.dream_world.front_default}"
+          <img src="${pokemon.photo}"
             alt="Foto de um ${pokemon.name}"/>
       </div>
     </li>
@@ -27,5 +19,6 @@ function convertPokemonHTML(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonHTML).join("");
+  const newHtml = pokemons.map(convertPokemonHTML).join("");
+  pokemonList.innerHTML = newHtml;
 });
